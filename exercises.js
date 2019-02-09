36b45c74c2502ec872e1e6e3e43dedcd3c0d8c44
Jess Manapul <Jess@Jess.home>
2019-02-08 20:54:24 -1000

    completed #1

--- exercises.js
+++ exercises.js
@@ -7,6 +7,7 @@ if(today === "Friday"){
   return "Let's Party!";
 };*/
 
+
 /*
 If/else statements = Evaluates (or checks) a condition. If the condition is true, the first code block is executed. If the condition is false, the second code block is executed instead.
 */
@@ -29,6 +30,13 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * The function will return true if the number passed into the function is equal to or greater than Hawaii's voting age. Console.log your result.
 */
 
+function canVote (age) {
+  if (age > 17) return true;
+  else return false;
+}
+
+console.log(canVote(17));
+console.log(canVote(18));
 
 /*
  * #2
@@ -41,6 +49,7 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * The function will return the message: "Login Success!", if the string passed into the function is "test1234"
  * Console.log your result.
 */
+
 
 
 /*
